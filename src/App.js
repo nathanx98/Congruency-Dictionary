b547d9d95c5c2152e8276f3cebad96c2621ce953
nathanx98 <xuanjiyang09@gmail.com>
2022-07-16 11:16:55 +0800

    set up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import profileImg from './img/myself.jpg'
+import logo from './img/logo.png'
 
 function App() {
   return (
-    <div>
-      <h1> My React App</h1>
-      <h2> Author: Nathan Xuan</h2>
+    <div className='App'>
+      <div className="navbar navbar-light">
+        <img src = {logo} id='logo'></img>
+      </div>
+
+      <div className='content'>
+        <h2> Creator: 大聪明</h2>
+        <p> ⬇️</p>
+        <img src = {profileImg} id='profile'></img>
+        
+      </div>
     </div>
     
   );
