93a03660cd2d54521a018c8bddffb8fde846a3b3
nathanx98 <xuanjiyang09@gmail.com>
2022-07-20 17:23:30 +0800

    .

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import './App.css';
 import profileImg from './img/myself.jpg'
 import logo from './img/logo.png'
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 
 function App() {
   return (
@@ -10,10 +15,16 @@ function App() {
       </div>
 
       <div className='content'>
-        <h2> Creator: 大聪明</h2>
-        <p> ⬇️</p>
-        <img src = {profileImg} id='profile'></img>
-        
+        {/* <h2> Creator: 大聪明</h2>
+        <p> ⬇️</p> */}
+        {/* <img src = {profileImg} id='profile'></img> */}
+
+        <Form>
+          <Form.Group className="mb-3" controlId="search bar">
+            <Form.Label>Enter an English word</Form.Label>
+            <Form.Control type="email" placeholder="Enter word here" />
+          </Form.Group>
+        </Form>
       </div>
     </div>
     
