3dd7b80b65c6b5d728a84130cfdc39d8b9652a09
nathanx98 <xuanjiyang09@gmail.com>
2022-07-21 09:07:27 +0800

    。

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import './App.css';
+import React, {useEffect, useState} from 'react';
 import profileImg from './img/myself.jpg'
 import logo from './img/logo.png'
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-
+import * as d3 from 'd3';
+import data from './data.csv'
+import Papa from 'papaparse'
 
 function App() {
+  // let d = {}
+  // d3.csv(data, function(data) {
+  //   // dict[[data['make a choice']]] = data['faire un choix']
+  //   let key = "make a choice"
+  //   let val = 'faire un choix'
+  //   Object.assign(d, {[data[key]]: data[val]})
+  //   setDict(d)
+  // })
+
+  let dict = {
+    "make a choice": "faire un choix",
+    'make an offer':'faire une offre',
+    'make a commitment':'prendre un engagement',
+    'make one\'s escape':'prendre la fuite',
+  }
+
+
   return (
     <div className='App'>
       <div className="navbar navbar-light">
